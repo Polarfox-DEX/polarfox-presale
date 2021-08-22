d5d7b59b6ad2f3cb3b11935d4469c11c2b7476d1
Clément Aguilar <clem@polarfox.io>
2021-08-22 16:44:42 +0200

    Clean VestedAirdrop code

--- scripts/presaleConstants.js
+++ scripts/presaleConstants.js
@@ -1,7 +1,7 @@
 const { IS_PRODUCTION } = require('./const')
 
 // TODO: Need to rewrite this so this works with the hardhat scripts
-const PFX = IS_PRODUCTION ? '' : '0x683FBa38e08e768981a42dB6425a01864cBA8d63'
+const PFX = IS_PRODUCTION ? '' : '0x5728Feb0ABF97938Bbd205fea7148c87d8721C3A'
 
 const NUMBER_OF_LEVELS = 120
 
